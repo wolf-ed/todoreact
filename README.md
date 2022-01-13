a559d36adcca447fb35e7f66106c263ea6670126
wolf-ed <wolf7ed@gmail.com>
2022-01-13 22:22:15 +0100

    todo app with react

---
+++ README.md
@@ -0,0 +1,20 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Author: Ed Wolf
+
+## Table of contents
+* [Introduction](#Introduction)
+* [Technologies](#Technologies)
+
+## Introduction
+This is a one page react app to add To-do tasks, with dynamic styled components.
+
+## Technologies
+
+* React- JavaScript
+* Node
+* HTML and CSS
+* VisualStudio Code
+* Git - GitHub
---
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
+import './CourseGoalList.css';
+
+
+//Parent Component: App
+const CourseGoalList = props => {
+  return (
+    <ul className="goal-list">
+      {props.items.map(goal => (
+        <CourseGoalItem
+          key={goal.id}
+          id={goal.id}
+          onDelete={props.onDeleteItem}
+        >
+          {goal.text}
+        </CourseGoalItem>
+      ))}
+    </ul>
+  );
+};
+
+export default CourseGoalList;
