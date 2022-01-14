bf87af8aaa86dc5fbbb18187ebf8450273f18bdc
wolf-ed <wolf7ed@gmail.com>
2022-01-14 12:45:41 +0100

    styles updated to use css modules

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Author: Ed Wolf
 
+## Link: *https://relaxed-almeida-016700.netlify.app*
+
 ## Table of contents
 * [Introduction](#Introduction)
 * [Technologies](#Technologies)
@@ -18,3 +20,4 @@ This is a one page react app to add To-do tasks, with dynamic styled components.
 * HTML and CSS
 * VisualStudio Code
 * Git - GitHub
+* Styled-components *https://styled-components.com/*
--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
+// import styled from 'styled-components';
+
 
 //Parent Component: App
 const CourseInput = props => {
@@ -27,7 +29,8 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+      {/* <FormControl invalid={!isValid}> */}
       {/* <div className='form-control'> */}
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} value={enteredValue}/>
@@ -38,3 +41,31 @@ const CourseInput = props => {
 };
 
 export default CourseInput;
+
+// const FormControl = styled.div`
+//  {
+//   margin: 0.5rem 0;
+// }
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => (props.invalid ? 'red' : 'black')};
+// }
+
+// & input {
+//   display: block;
+//   width: 100%;
+//   border:  solid ${props => (props.invalid ? '3px red' : '1px #ccc')};
+//   background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
+
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
+// `;
